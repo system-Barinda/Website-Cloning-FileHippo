d8a4f7bb8114042fd9fa80e120d4e04bde2e4730
system sylvere BARINDA <systembarinda@gmail.com>
2025-08-15 17:34:32 +0200

    the time-box container now it is done

--- filehippo/src/App.jsx
+++ filehippo/src/App.jsx
@@ -11,6 +11,11 @@ function App() {
     <>
       <HeaderMenu />
       <MinHeader />
+      <div className="timeruning-box bg-blue-600 text-white flex h-20 justify-between w-full  items-center  ">
+         <div className="right-time border w-[30%] grow h-auto content-center text-center" ><p>3535345,3435</p><p>Downloads served</p></div>
+         <div className="right-time border w-[30%] grow h-auto content-center text-center"><p>2 minutes ago</p><p>Last update check</p></div>
+         <div className="right-time border w-[30%] grow h-auto content-center text-center"><p>43,490</p><p>Program versions listed</p></div>
+      </div>
     </>
   )
 }
