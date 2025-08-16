eafd6f3f4dec381d60d8cc3b8943879be54a818a
system sylvere BARINDA <systembarinda@gmail.com>
2025-08-16 22:52:44 +0200

    I started to design the side of  categories with header and flex

---
+++ filehippo/src/components/containerApp.jsx
@@ -0,0 +1,13 @@
+function ContainerApp(){
+    return (
+        <>
+        <div className="container-box-App border h-200 w-full flex">
+             <div className="cotegories border m-1 h-auto w-1/2">
+               <h1 className="ml-8 font-bold text-2xl">Categories</h1>
+             </div>
+             <div className="popular border m-1 h-auto w-1/2"></div>
+        </div>
+        </>
+    );
+}
+export default ContainerApp;
