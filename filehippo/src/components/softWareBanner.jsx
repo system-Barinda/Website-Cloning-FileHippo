1988e746d15c8d9c8789db9b94db1097ff964120
system sylvere BARINDA <systembarinda@gmail.com>
2025-08-18 19:17:03 +0200

    the all the  item of box

--- filehippo/src/components/softWareBanner.jsx
+++ filehippo/src/components/softWareBanner.jsx
@@ -3,9 +3,54 @@ function SoftWareBanner(){
 
     return(
         <>
-        <div className="border h-200 w-full flex">
+        <div className="border h-450 w-full flex">
            <div className="soft h-auto w-[70%] border m-1 flex flex-wrap">
              
+                       <div className="item h-100 w-100 border flex flex-col gap-2 m-1">
+                          <img src={laptop} className='h-[60%] w-auto'/>
+                          <p className=''>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta repellat 
+                            nisi nostrum dolorum quidem vero veritatis consequatur porro possimus dolores voluptas
+                             culpa doloremque ab voluptatem cum aspernatur, illo soluta excepturi?
+                          </p>
+                       </div>
+
+                       <div className="item h-100 w-100 border flex flex-col gap-2 m-1">
+                          <img src={laptop} className='h-[60%] w-auto'/>
+                          <p className=''>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta repellat 
+                            nisi nostrum dolorum quidem vero veritatis consequatur porro possimus dolores voluptas
+                             culpa doloremque ab voluptatem cum aspernatur, illo soluta excepturi?
+                          </p>
+                       </div>
+
+                       <div className="item h-100 w-100 border flex flex-col gap-2 m-1">
+                          <img src={laptop} className='h-[60%] w-auto'/>
+                          <p className=''>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta repellat 
+                            nisi nostrum dolorum quidem vero veritatis consequatur porro possimus dolores voluptas
+                             culpa doloremque ab voluptatem cum aspernatur, illo soluta excepturi?
+                          </p>
+                       </div>
+
+                       <div className="item h-100 w-100 border flex flex-col gap-2 m-1">
+                          <img src={laptop} className='h-[60%] w-auto'/>
+                          <p className=''>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta repellat 
+                            nisi nostrum dolorum quidem vero veritatis consequatur porro possimus dolores voluptas
+                             culpa doloremque ab voluptatem cum aspernatur, illo soluta excepturi?
+                          </p>
+                       </div>
+
+                       <div className="item h-100 w-100 border flex flex-col gap-2 m-1">
+                          <img src={laptop} className='h-[60%] w-auto'/>
+                          <p className=''>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta repellat 
+                            nisi nostrum dolorum quidem vero veritatis consequatur porro possimus dolores voluptas
+                             culpa doloremque ab voluptatem cum aspernatur, illo soluta excepturi?
+                          </p>
+                       </div>
+
                        <div className="item h-100 w-100 border flex flex-col gap-2 m-1">
                           <img src={laptop} className='h-[60%] w-auto'/>
                           <p className=''>
